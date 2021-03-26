0a9d6a862dc27277a8673000db2c4edadc22ac28
elina <e_petrova@fanshaweonline.ca>
2021-03-25 21:16:58 -0400

    clickable bottom gallery teampage

--- public/js/components/teamGalleryImg.js
+++ public/js/components/teamGalleryImg.js
@@ -2,7 +2,25 @@ export default {
     props: ['img'],
     template: `
     <div>
-    <img :src="img.src" alt="LRG gallery image" >
+    <img v-on:click="changeImage($event)" :src="img.src" alt="LRG gallery image" >
     </div>
-    `
+    `,
+    methods: {
+        changeImage(event) {
+            console.log(event.target.parentElement.classList);
+            if (event.target.parentElement.classList.contains("right_one")) {
+                console.log('right');
+                this.slideNext();
+            } else if (event.target.parentElement.classList.contains("left_one")) {
+                console.log('left');
+                this.slideBack();
+            }
+        },
+        slideNext() {
+            this.$parent.slideNext();
+        },
+        slideBack() {
+            this.$parent.slideBack();
+        }
+    }
 }
